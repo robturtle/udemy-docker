24848a5018aa4c36a4a0d8dfd6a604b379cdc91c
Yang Liu <jeremyrobturtle@gmail.com>
2019-09-14 12:44:22 -0700

    [APP][ROUTE] setup router

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class OtherPage extends Component {
+  render() {
+    return (
+      <p>
+        This is other page.
+        <a href="/">return home</a>
+      </p>
+    );
+  }
+}
